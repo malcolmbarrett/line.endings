cc2c3f76ca983222c1e5248848dd552b05be6020
Malcolm Barrett <malcolmbarrett@gmail.com>
2021-04-28 16:11:34 -0700

    try this

--- R/add_lines.R
+++ R/add_lines.R
@@ -4,7 +4,7 @@ add_lines <- function() {
   "
   line_ending <- if (.Platform$OS.type == "windows") "\r\n" else "\n"
   x <- gsub("\r?\n", line_ending, x)
-  brio::write_lines(x, "x_brio.txt", eol = line_ending)
+  brio::write_lines(enc2utf8(x), "x_brio.txt", eol = line_ending)
   withr::local_options(usethis.overwrite = TRUE)
   usethis::write_over("x_usethis.txt", x)
 }
